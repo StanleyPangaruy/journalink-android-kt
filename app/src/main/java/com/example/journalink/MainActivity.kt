b88892bc166fb1a41325d13ee19ff12aa638126f
Panda_Tine <92630250+christineilagan02@users.noreply.github.com>
2023-06-19 04:57:58 +0800

    i have set the setOnClickListener to switch page to another page.

--- app/src/main/java/com/example/journalink/MainActivity.kt
+++ app/src/main/java/com/example/journalink/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.journalink
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val signUpBtn = findViewById<Button>(R.id.signUpButton)
+        signUpBtn.setOnClickListener {
+            val intent = Intent (this, SignUp:: class.java)
+            startActivity(intent)
+        }
+
+        val forgotPasswordTxt = findViewById<TextView>(R.id.forgotPasswordText)
+        forgotPasswordTxt.setOnClickListener {
+            val intent = Intent(this, ForgotPassword::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/journalink/SignUp.kt
@@ -0,0 +1,14 @@
+package com.example.journalink
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up_page)
+
+        // Add other activity code here
+    }
+}
