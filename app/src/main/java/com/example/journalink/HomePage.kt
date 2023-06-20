c86452467b274833064f2bebf418dc7682a0b6e7
Carlos Fidel Rosales <fidelcarlos302@gmail.com>
2023-06-20 10:28:22 +0800

    Deleted the MainActivity and activitymain and changed it to HomePage

--- app/src/main/java/com/example/journalink/HomePage.kt
+++ app/src/main/java/com/example/journalink/HomePage.kt
@@ -3,9 +3,9 @@ package com.example.journalink
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home_page)
     }
 }
