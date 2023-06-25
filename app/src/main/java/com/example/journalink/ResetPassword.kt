cc47e9fff52a12dc9b6a8ce1be04948e375148af
Panda_Tine <92630250+christineilagan02@users.noreply.github.com>
2023-06-25 16:18:32 +0800

    Done fixing the error and I already connect the confirm button to login page

--- app/src/main/java/com/example/journalink/ResetPassword.kt
+++ app/src/main/java/com/example/journalink/ResetPassword.kt
@@ -1,6 +1,8 @@
 package com.example.journalink
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class ResetPassword : AppCompatActivity() {
@@ -8,6 +10,12 @@ class ResetPassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.reset_pw)
+
+        val confirmBtn = findViewById<ImageButton>(R.id.confirmButton)
+        confirmBtn.setOnClickListener {
+            val intent = Intent (this, MainActivity:: class.java)
+            startActivity(intent)
+        }
 
         // Add other activity code here
     }
