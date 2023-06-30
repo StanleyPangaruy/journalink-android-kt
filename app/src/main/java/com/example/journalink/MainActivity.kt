c816330eb4700b062b508dae182655787409f5c8
Stanley John O. Pangaruy <pangaruystanley@GMAIL.COM>
2023-06-30 22:20:34 +0800

    sign up function fix
    implemented register function and remade firebase auth

--- app/src/main/java/com/example/journalink/MainActivity.kt
+++ app/src/main/java/com/example/journalink/MainActivity.kt
@@ -1,110 +1,52 @@
 package com.example.journalink
-
+import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import android.widget.Toast
+import com.example.journalink.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-    var loginUsername: EditText? = null
-    var loginPassword: EditText? = null
-    var loginButton: Button? = null
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    // Perform login with email or username
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        loginUsername = findViewById(R.id.usernameEditText)
-        loginPassword = findViewById(R.id.passwordEditText)
-        val loginButton = findViewById<ImageButton>(R.id.loginButton)
-        loginButton.setOnClickListener {
-            if (!validateUsername() or !validatePassword()) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.loginButton.setOnClickListener {
+            val email = binding.usernameEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
             } else {
-                checkUser()
+                Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
             }
         }
+
+
         val signUpBtn = findViewById<ImageButton>(R.id.signUpButton)
         signUpBtn.setOnClickListener {
             val intent = Intent (this, SignUp:: class.java)
             startActivity(intent)
         }
-
         val forgotPasswordTxt = findViewById<TextView>(R.id.forgotPasswordText)
         forgotPasswordTxt.setOnClickListener {
             val intent = Intent(this, ForgotPassword::class.java)
             startActivity(intent)
         }
-    }
-
-    private fun validateUsername(): Boolean {
-        val `val` = loginUsername!!.text.toString()
-        return if (`val`.isEmpty()) {
-            loginUsername!!.error = "Username cannot be empty"
-            false
-        } else {
-            loginUsername!!.error = null
-            true
-        }
-    }
-
-    private fun validatePassword(): Boolean {
-        val `val` = loginPassword!!.text.toString()
-        return if (`val`.isEmpty()) {
-            loginPassword!!.error = "Password cannot be empty"
-            false
-        } else {
-            loginPassword!!.error = null
-            true
-        }
-    }
-
-    private fun checkUser() {
-        val userUsername = loginUsername!!.text.toString().trim { it <= ' ' }
-        val userPassword = loginPassword!!.text.toString().trim { it <= ' ' }
-        val reference = FirebaseDatabase.getInstance().getReference("users")
-        val checkUserDatabase = reference.orderByChild("username").equalTo(userUsername)
-        checkUserDatabase.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                if (snapshot.exists()) {
-                    loginUsername!!.error = null
-                    val passwordFromDB = snapshot.child(userUsername).child("password").getValue(
-                        String::class.java
-                    )
-                    if (passwordFromDB == userPassword) {
-                        loginUsername!!.error = null
-                        val nameFromDB = snapshot.child(userUsername).child("name").getValue(
-                            String::class.java
-                        )
-                        val emailFromDB = snapshot.child(userUsername).child("email").getValue(
-                            String::class.java
-                        )
-                        val usernameFromDB =
-                            snapshot.child(userUsername).child("username").getValue(
-                                String::class.java
-                            )
-                        val intent = Intent(this@MainActivity, MainActivity::class.java)
-                        intent.putExtra("name", nameFromDB)
-                        intent.putExtra("email", emailFromDB)
-                        intent.putExtra("username", usernameFromDB)
-                        intent.putExtra("password", passwordFromDB)
-                        startActivity(intent)
-                    } else {
-                        loginPassword!!.error = "Invalid Credentials"
-                        loginPassword!!.requestFocus()
-                    }
-                } else {
-                    loginUsername!!.error = "User does not exist"
-                    loginUsername!!.requestFocus()
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {}
-        })
     }
 }
--- app/src/main/java/com/example/journalink/SignUp.kt
+++ app/src/main/java/com/example/journalink/SignUp.kt
@@ -3,67 +3,44 @@ package com.example.journalink
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageButton
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
 import com.example.journalink.databinding.SignUpPageBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 
 class SignUp : AppCompatActivity() {
+
     private lateinit var binding: SignUpPageBinding
     private lateinit var firebaseAuth: FirebaseAuth
-    private var signupName: EditText? = null
-    private var signupUsername: EditText? = null
-    private var signupEmail: EditText? = null
-    private var signupPassword: EditText? = null
-    private var signupButton: Button? = null
-    private var database: FirebaseDatabase? = null
-    private var reference: DatabaseReference? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.sign_up_page)
         binding = SignUpPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         firebaseAuth = FirebaseAuth.getInstance()
-        signupName = findViewById(R.id.completeNameEditText)
-        signupEmail = findViewById(R.id.emailEditText)
-        signupUsername = findViewById(R.id.usernameEditText)
-        signupPassword = findViewById(R.id.passwordEditText)
-        val signupButton = findViewById<ImageButton>(R.id.registerButton)
-        signupButton.setOnClickListener {
-            database = FirebaseDatabase.getInstance()
-            reference = database!!.getReference("users")
-            val name = signupName!!.text.toString()
-            val email = signupEmail!!.text.toString()
-            val username = signupUsername!!.text.toString()
-            val password = signupPassword!!.text.toString()
+        binding.registerButton.setOnClickListener{
+            println("test")
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
             val confirmPassword = binding.confirmPasswordEditText.text.toString()
             if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
                 if (password == confirmPassword){
                     firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
                         if (it.isSuccessful){
-                            val intent = Intent(this, MainActivity::class.java)
+                            val intent = Intent(this@SignUp, MainActivity::class.java)
                             startActivity(intent)
+                            finish()
+                            Toast.makeText(this, "Pogi ni Fidel", Toast.LENGTH_SHORT).show()
                         } else {
                             Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
                         }
                     }
                 } else {
-                    Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
-                }}
-            val helperClass = HelperClass(
-                name = name,
-                email = email,
-                username = username,
-                password = password
-            )
-            reference!!.child(username).setValue(helperClass)
-            Toast.makeText(this@SignUp, "You have signed up successfully!", Toast.LENGTH_SHORT)
-                .show()
-            val intent = Intent(this@SignUp, MainActivity::class.java)
-            startActivity(intent)
+                    Toast.makeText(this, "Password does not matched", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            }
         }
 
         val arrowBtn = findViewById<ImageButton>(R.id.arrow)
@@ -71,7 +48,6 @@ class SignUp : AppCompatActivity() {
             val intent = Intent (this, MainActivity:: class.java)
             startActivity(intent)
         }
-
         // Add other activity code here
     }
 }
