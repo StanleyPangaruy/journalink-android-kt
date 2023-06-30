87ace1e708b4942b96069c8aac2468748ea318d6
Panda_Tine <92630250+christineilagan02@users.noreply.github.com>
2023-06-30 18:02:16 +0800

    Done removing the username textbox in sign up and changing the whole layout of forgot password and reset password

--- app/src/main/java/com/example/journalink/ForgotPassword.kt
+++ app/src/main/java/com/example/journalink/ForgotPassword.kt
@@ -2,24 +2,24 @@ package com.example.journalink
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ForgotPassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.forgot_pw_code)
+        setContentView(R.layout.forgot_pw)
 
-        val verifyBtn = findViewById<ImageButton>(R.id.verifyButton)
-        verifyBtn.setOnClickListener {
+        val sendBtn = findViewById<ImageButton>(R.id.sendButton)
+        sendBtn.setOnClickListener {
             val intent = Intent (this, ResetPassword:: class.java)
             startActivity(intent)
         }
 
-        val arrowBtn = findViewById<ImageButton>(R.id.arrow)
-        arrowBtn.setOnClickListener {
-            val intent = Intent (this, MainActivity:: class.java)
+        val loginBtn = findViewById<TextView>(R.id.loginText)
+        loginBtn.setOnClickListener {
+            val intent = Intent (this, ResetPassword:: class.java)
             startActivity(intent)
         }
     }
