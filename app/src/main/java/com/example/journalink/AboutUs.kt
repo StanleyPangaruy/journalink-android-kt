1dbc4d54d06560e7838a3c5216b5910e3e1d0c0a
Pidel <fidelcarlos302@gmail.com>
2023-07-01 14:51:53 +0800

    Added intent function to the kotlin files to be able to navigate through the settings page.

--- app/src/main/java/com/example/journalink/AboutUs.kt
+++ app/src/main/java/com/example/journalink/AboutUs.kt
@@ -1,11 +1,19 @@
 package com.example.journalink
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class AboutUs : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_us)
+
+    var returntoSetting = findViewById<ImageButton>(R.id.closebutton)
+    returntoSetting.setOnClickListener{
+        val intent1 = Intent(this, SettingsPage::class.java)
+        startActivity(intent1)
+        }
     }
 }
--- app/src/main/java/com/example/journalink/SettingsPage.kt
+++ app/src/main/java/com/example/journalink/SettingsPage.kt
@@ -1,11 +1,44 @@
 package com.example.journalink
 
+
+
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+
+
 
 class SettingsPage : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings_page)
+
+
+        val gmailButton: ImageButton = findViewById(R.id.contactUs)
+        gmailButton.setOnClickListener {
+            val recipient = "imaginativevtechnologies@gmail.com"
+            val subject = ""
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            startActivity(intent)
+        }
+
+        var privacypolicy = findViewById<ImageButton>(R.id.privacyPolicybtn)
+        privacypolicy.setOnClickListener {
+            val intent1 = Intent(this, PrivacyPolicy::class.java)
+            startActivity(intent1)
+        }
+
+        var about = findViewById<ImageButton>(R.id.aboutBtn)
+        about.setOnClickListener{
+            val intent2 = Intent(this, AboutUs::class.java)
+            startActivity(intent2)
+        }
+
     }
 }
