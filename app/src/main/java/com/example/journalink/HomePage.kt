52598ad00be96e4be2ea862375e6c6537dfef5f9
Stanley John O. Pangaruy <pangaruystanley@GMAIL.COM>
2023-07-02 18:40:37 +0800

    merged home, create and view
    Merged multiple branches

--- app/src/main/java/com/example/journalink/HomePage.kt
+++ app/src/main/java/com/example/journalink/HomePage.kt
@@ -3,16 +3,9 @@ package com.example.journalink
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-<<<<<<<< HEAD:app/src/main/java/com/example/journalink/HomePage.kt
 class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
-========
-class CreateJournal : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_journal)
->>>>>>>> createJournalpage:app/src/main/java/com/example/journalink/CreateJournal.kt
     }
 }
--- app/src/main/java/com/example/journalink/ViewJournal.kt
+++ app/src/main/java/com/example/journalink/ViewJournal.kt
@@ -3,10 +3,9 @@ package com.example.journalink
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-class CreateJournal : AppCompatActivity() {
+class ViewJournal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_journal)
+        setContentView(R.layout.activity_view_journal)
     }
 }
