8fa8d3639d49d6980d502109613259bdcffc4af2
Stanley John O. Pangaruy <pangaruystanley@GMAIL.COM>
2023-07-02 22:37:14 +0800

    Implemented forgot password
    allows user to change their password through email and external web browser. added few lines of codes to homepage/ login in fix incoming

--- app/src/main/java/com/example/journalink/ForgotPassword.kt
+++ app/src/main/java/com/example/journalink/ForgotPassword.kt
@@ -2,25 +2,40 @@ package com.example.journalink
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class ForgotPassword : AppCompatActivity() {
+    private lateinit var etPassword: EditText
+    private lateinit var sendBtnPassword: ImageButton
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.forgot_pw)
 
-        val sendBtn = findViewById<ImageButton>(R.id.sendButton)
-        sendBtn.setOnClickListener {
-            val intent = Intent (this, ResetPassword:: class.java)
-            startActivity(intent)
-        }
+        etPassword = findViewById(R.id.emailEditText)
+        sendBtnPassword = findViewById(R.id.sendButton)
 
-        val loginBtn = findViewById<TextView>(R.id.loginText)
-        loginBtn.setOnClickListener {
-            val intent = Intent (this, Login:: class.java)
-            startActivity(intent)
+        auth = FirebaseAuth.getInstance()
+
+        sendBtnPassword.setOnClickListener {
+            val sPassword = etPassword.text.toString()
+            auth.sendPasswordResetEmail(sPassword)
+                .addOnSuccessListener {
+                    Toast.makeText(this,"Please Check your Email!", Toast.LENGTH_SHORT).show()
+                    val intent = Intent (this, ResetPassword:: class.java)
+                    startActivity(intent)
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+                }
         }
     }
 }
+
--- app/src/main/java/com/example/journalink/HomePage.kt
+++ app/src/main/java/com/example/journalink/HomePage.kt
@@ -2,10 +2,13 @@ package com.example.journalink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.journalink.databinding.ActivityHomePageBinding
 
 class HomePage : AppCompatActivity() {
+    private lateinit var binding: ActivityHomePageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
