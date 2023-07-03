a13a17cb289b11915d0b2f27c03b5a3693a6535a
Gian Carlo Estrella <giancarloestrella09@gmail.com>
2023-07-03 19:11:49 +0800

    Update ViewJournal.kt

--- app/src/main/java/com/example/journalink/ViewJournal.kt
+++ app/src/main/java/com/example/journalink/ViewJournal.kt
@@ -1,11 +1,14 @@
 package com.example.journalink
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class ViewJournal : AppCompatActivity() {
+
+
+class viewJournal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_journal)
+
     }
 }
