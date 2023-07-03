2bd1d003217625b6d8a77c072dfbe15e6f9e6a3c
Gian Carlo Estrella <giancarloestrella09@gmail.com>
2023-07-03 18:53:32 +0800

    Update merged files

--- app/src/main/java/com/example/journalink/viewJournal.kt
+++ app/src/main/java/com/example/journalink/viewJournal.kt
@@ -1,18 +1,13 @@
 package com.example.journalink
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-<<<<<<<< HEAD:app/src/main/java/com/example/journalink/CreateJournal.kt
-class CreateJournal : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_journal)
-========
+
 class viewJournal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_journal)
->>>>>>>> origin/viewJournalpage:app/src/main/java/com/example/journalink/viewJournal.kt
+
     }
 }
