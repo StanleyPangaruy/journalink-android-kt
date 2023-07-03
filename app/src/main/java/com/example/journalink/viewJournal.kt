0a52a0dc6bc57f4a463624b27f79b0132c195a82
Gian Carlo Estrella <giancarloestrella09@gmail.com>
2023-07-03 18:57:56 +0800

    Update merged files

--- app/src/main/java/com/example/journalink/viewJournal.kt
+++ app/src/main/java/com/example/journalink/viewJournal.kt
@@ -3,18 +3,12 @@ package com.example.journalink
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-<<<<<<<< HEAD:app/src/main/java/com/example/journalink/viewJournal.kt
+
 
 class viewJournal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_journal)
 
-========
-class TermsandConditions : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_termsand_conditions)
->>>>>>>> terms&conditionsPage:app/src/main/java/com/example/journalink/TermsandConditions.kt
     }
 }
