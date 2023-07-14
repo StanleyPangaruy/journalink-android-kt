983b3dea36366be40979de972c7802260a99519c
Panda_Tine <92630250+christineilagan02@users.noreply.github.com>
2023-07-14 11:44:12 +0800

    i change the firestore into realtime database

--- app/src/main/java/com/example/journalink/Journal.kt
+++ app/src/main/java/com/example/journalink/Journal.kt
@@ -1,11 +1,25 @@
 package com.example.journalink
 
+import java.text.SimpleDateFormat
 import java.util.*
 
 data class Journal(
+    val id: String = "",
     val title: String = "",
     val shortDescription: String = "",
     val content: String = "",
     val uid: String = "",
     val timestamp: Date = Date()
-)
+) {
+    val date: String
+        get() {
+            val format = SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault())
+            return format.format(timestamp)
+        }
+
+    val time: String
+        get() {
+            val format = SimpleDateFormat("HH:mm", Locale.getDefault())
+            return format.format(timestamp)
+        }
+}
--- app/src/main/java/com/example/journalink/ViewJournal.kt
+++ app/src/main/java/com/example/journalink/ViewJournal.kt
@@ -4,59 +4,56 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
-
 
 class ViewJournal : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
-    private lateinit var journalEntry: JournalEntry
+    private lateinit var adapter: RVAdapter
     private lateinit var auth: FirebaseAuth
-    private lateinit var adaptor: RVAdaptor
+    private lateinit var database: FirebaseDatabase
+    private lateinit var journalRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_journal)
 
         recyclerView = findViewById(R.id.recyclerView)
-
-        journalEntry = JournalEntry()
         auth = Firebase.auth
+        database = FirebaseDatabase.getInstance()
+        journalRef = database.reference.child("journals")
 
         setUpRecyclerView()
-
     }
 
     private fun setUpRecyclerView() {
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        // get journalCollection collection from journalEntry
-        // get currentUserId using auth
-        val journalCollection = journalEntry.journalCollection
-        val currentUserId = auth.currentUser!!.uid
+        val currentUserId = auth.currentUser?.uid
 
-        // create a query in noteCollection
-        val query = journalCollection.whereEqualTo("uid",currentUserId).orderBy("timestamp")
+        val query = journalRef.orderByChild("uid").equalTo(currentUserId)
 
-        // create a firestoreRecyclerViewOption
-        val recyclerViewOption = FirestoreRecyclerOptions.Builder<Journal>().setQuery(query,Journal::class.java).build()
+        val options = FirebaseRecyclerOptions.Builder<Journal>()
+            .setQuery(query, Journal::class.java)
+            .build()
 
-        // add adaptor
-        adaptor = RVAdaptor(recyclerViewOption)
-        recyclerView.adapter = adaptor
+        adapter = RVAdapter(options)
+        recyclerView.adapter = adapter
     }
+
 
     override fun onStart() {
         super.onStart()
-        adaptor.startListening()
+        adapter.startListening()
     }
 
     override fun onStop() {
         super.onStop()
-        adaptor.stopListening()
+        adapter.stopListening()
     }
-
 }
