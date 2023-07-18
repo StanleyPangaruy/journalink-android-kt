f112d21fa58662bd433b0285ee64ba07fe497d6b
Stanley John O. Pangaruy <pangaruystanley@GMAIL.COM>
2023-07-18 16:18:13 +0800

    naming

--- app/src/main/java/com/example/journalink/journalFeed.kt
+++ app/src/main/java/com/example/journalink/journalFeed.kt
@@ -3,17 +3,9 @@ package com.example.journalink
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-<<<<<<<< HEAD:app/src/main/java/com/example/journalink/TermsandConditions.kt
-
-class TermsandConditions : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_termsand_conditions)
-========
 class journalFeed : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_journal_feed)
->>>>>>>> journalFeed:app/src/main/java/com/example/journalink/journalFeed.kt
     }
 }
