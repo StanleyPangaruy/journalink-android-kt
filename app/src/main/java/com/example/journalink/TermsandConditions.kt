94bce3717b85b7a72d0f6dde6eb1d1277ff85d16
Stanley John O. Pangaruy <pangaruystanley@GMAIL.COM>
2023-07-18 15:59:15 +0800

    merge and modify
    merged the journal feed branch to saving journal to work on the journal feed db be

--- app/src/main/java/com/example/journalink/TermsandConditions.kt
+++ app/src/main/java/com/example/journalink/TermsandConditions.kt
@@ -3,17 +3,10 @@ package com.example.journalink
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-<<<<<<<< HEAD:app/src/main/java/com/example/journalink/journalFeed.kt
-class journalFeed : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_journal_feed)
-========
 
 class TermsandConditions : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_termsand_conditions)
->>>>>>>> SavingJournal2:app/src/main/java/com/example/journalink/TermsandConditions.kt
     }
 }
