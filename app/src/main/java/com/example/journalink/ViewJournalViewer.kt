700df041f7690bf1ed04b5014b4d285dc394f1f5
Panda_Tine <92630250+christineilagan02@users.noreply.github.com>
2023-07-23 00:43:22 +0800

    I add the edit function

--- app/src/main/java/com/example/journalink/ViewJournalViewer.kt
+++ app/src/main/java/com/example/journalink/ViewJournalViewer.kt
@@ -2,19 +2,22 @@ package com.example.journalink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.database.FirebaseDatabase
 
 class ViewJournalViewer : AppCompatActivity() {
 
-    private lateinit var journTitle : TextView
-    private lateinit var journshortDesc : TextView
-    private lateinit var journContent : TextView
-    private lateinit var shareBUTTON : ImageButton
-    private lateinit var closeJournal : ImageButton
-    private lateinit var deleteBUTTON : ImageButton
-    private lateinit var editBUTTON : ImageButton
-
+    private lateinit var journTitle: TextView
+    private lateinit var journshortDesc: TextView
+    private lateinit var journContent: TextView
+    private lateinit var shareBUTTON: ImageButton
+    private lateinit var closeJournal: ImageButton
+    private lateinit var deleteBUTTON: ImageButton
+    private lateinit var editBUTTON: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +25,10 @@ class ViewJournalViewer : AppCompatActivity() {
 
         initView()
         setValuesToViews()
+
+        editBUTTON.setOnClickListener {
+            openEditDialog()
+        }
     }
 
     private fun initView() {
@@ -37,5 +44,58 @@ class ViewJournalViewer : AppCompatActivity() {
         journTitle.text = intent.getStringExtra("title")
         journshortDesc.text = intent.getStringExtra("shortDescription")
         journContent.text = intent.getStringExtra("content")
+    }
+
+    private fun openEditDialog() {
+        val mDialog = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val mDialogView = inflater.inflate(R.layout.edit_dialog, null)
+
+        mDialog.setView(mDialogView)
+
+        val titleEditdialog = mDialogView.findViewById<EditText>(R.id.titleEditdialog)
+        val shortdescEditdialog = mDialogView.findViewById<EditText>(R.id.shortdescEditdialog)
+        val contentEditdialog = mDialogView.findViewById<EditText>(R.id.contentEditdialog)
+
+        val updatebtn = mDialogView.findViewById<ImageButton>(R.id.updatebtn)
+
+        titleEditdialog.setText(intent.getStringExtra("title").toString())
+        shortdescEditdialog.setText(intent.getStringExtra("shortDescription").toString())
+        contentEditdialog.setText(intent.getStringExtra("content").toString())
+
+        val alertDialog = mDialog.create()
+        alertDialog.show()
+
+        updatebtn.setOnClickListener {
+            val id = intent.getStringExtra("id").toString()
+
+            updateJournalEntry(
+                id,
+                titleEditdialog.text.toString(),
+                shortdescEditdialog.text.toString(),
+                contentEditdialog.text.toString()
+            )
+
+            Toast.makeText(applicationContext, "Journal Entry Updated", Toast.LENGTH_LONG).show()
+
+            journTitle.text = titleEditdialog.text.toString()
+            journshortDesc.text = shortdescEditdialog.text.toString()
+            journContent.text = contentEditdialog.text.toString()
+
+            alertDialog.dismiss()
+
+        }
+    }
+
+    private fun updateJournalEntry(
+        id: String,
+        title: String,
+        shortDescription: String,
+        content: String
+    ) {
+        val dbRef = FirebaseDatabase.getInstance().getReference("journals").child(id)
+        val journalData = Journal(id, title, shortDescription, content)
+
+        dbRef.setValue(journalData)
     }
 }
