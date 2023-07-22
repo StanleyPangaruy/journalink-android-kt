bbcee01fd81e77bee366556a391a5ecc4f72c3d4
Stanley John O. Pangaruy <pangaruystanley@GMAIL.COM>
2023-07-23 01:43:14 +0800

    Refactored journal feed and content viewer

--- app/src/main/java/com/example/journalink/JournalFeed.kt
+++ app/src/main/java/com/example/journalink/JournalFeed.kt
@@ -3,7 +3,7 @@ package com.example.journalink
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class journalFeed : AppCompatActivity() {
+class JournalFeed : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_journal_feed)
