95751f6dad31313c42a9672b096e8792fc78faa0
Stanley John O. Pangaruy <pangaruystanley@GMAIL.COM>
2023-07-23 00:23:30 +0800

    merged journal feed to test

--- app/src/main/java/com/example/journalink/journalContentViewer.kt
+++ app/src/main/java/com/example/journalink/journalContentViewer.kt
@@ -3,16 +3,9 @@ package com.example.journalink
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-<<<<<<<< HEAD:app/src/main/java/com/example/journalink/journalFeed.kt
-class journalFeed : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_journal_feed)
-========
 class journalContentViewer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_journal_content_viewer)
->>>>>>>> journalFeedContentViewer:app/src/main/java/com/example/journalink/journalContentViewer.kt
     }
 }
