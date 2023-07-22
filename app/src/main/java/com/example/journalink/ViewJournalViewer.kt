1b6b8bffddafa96b99a3796275ac72996c8c7961
Stanley John O. Pangaruy <pangaruystanley@GMAIL.COM>
2023-07-22 23:40:47 +0800

    share button implemented
    upon pressing the share button, the journal content will be passed to the realtime database and in a new separate node for all of the shared journals

--- app/src/main/java/com/example/journalink/ViewJournalViewer.kt
+++ app/src/main/java/com/example/journalink/ViewJournalViewer.kt
@@ -1,20 +1,26 @@
 package com.example.journalink
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class ViewJournalViewer : AppCompatActivity() {
 
-    private lateinit var journTitle : TextView
-    private lateinit var journshortDesc : TextView
-    private lateinit var journContent : TextView
-    private lateinit var shareBUTTON : ImageButton
-    private lateinit var closeJournal : ImageButton
-    private lateinit var deleteBUTTON : ImageButton
-    private lateinit var editBUTTON : ImageButton
+    private lateinit var journTitle: TextView
+    private lateinit var journshortDesc: TextView
+    private lateinit var journContent: TextView
+    private lateinit var shareBUTTON: ImageButton
+    private lateinit var closeJournal: ImageButton
+    private lateinit var deleteBUTTON: ImageButton
+    private lateinit var editBUTTON: ImageButton
 
+    private lateinit var journalId: String // Assuming you have the journal ID available
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,10 @@ class ViewJournalViewer : AppCompatActivity() {
 
         initView()
         setValuesToViews()
+
+        shareBUTTON.setOnClickListener {
+            shareJournal()
+        }
     }
 
     private fun initView() {
@@ -30,6 +40,7 @@ class ViewJournalViewer : AppCompatActivity() {
         journContent = findViewById(R.id.journContent)
 
         editBUTTON = findViewById(R.id.editBUTTON)
+        shareBUTTON = findViewById(R.id.shareBUTTON)
         deleteBUTTON = findViewById(R.id.deleteBUTTON)
     }
 
@@ -37,5 +48,37 @@ class ViewJournalViewer : AppCompatActivity() {
         journTitle.text = intent.getStringExtra("title")
         journshortDesc.text = intent.getStringExtra("shortDescription")
         journContent.text = intent.getStringExtra("content")
+
+        journalId =
+            intent.getStringExtra("journalId").toString() // Assuming you have the journal ID as an extra in the intent
+    }
+
+    private fun shareJournal() {
+        val title = journTitle.text.toString()
+        val shortDescription = journshortDesc.text.toString()
+        val date = getCurrentDate() // Replace this with the actual date value you want to use
+
+        // Store the shared data in Firebase Realtime Database under the specific journal ID
+        val databaseReference = FirebaseDatabase.getInstance().getReference("shared_journals").push()
+        val newJournalId = databaseReference.key.toString()
+        val data = mapOf(
+            "title" to title,
+            "shortDescription" to shortDescription,
+            "date" to date
+        )
+
+        databaseReference.setValue(data)
+            .addOnSuccessListener {
+                // Data shared successfully
+            }
+            .addOnFailureListener {
+                // Failed to share data, handle the error
+            }
+    }
+
+    // Function to get the current date (replace this with your actual date logic)
+    private fun getCurrentDate(): String {
+        val dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault())
+        return dateFormat.format(Date())
     }
 }
